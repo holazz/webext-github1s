528e58292e792b6a1e2228030a10e7cdf711edf1
holazz <2418184580@qq.com>
2022-05-12 15:26:56 +0800

    fix: remove pjax listener

--- content-script.js
+++ content-script.js
@@ -1,39 +1,36 @@
-async function init() {
-  if (!isRepoPage()) return
-
-  console.log('[GitHub1s] init')
-  const options = await chrome.storage.sync.get()
-  observeElement(options)
-  document.addEventListener('pjax:end', () => observeElement(options))
+function isRepoPage() {
+  return document.body.contains(document.querySelector('#repo-content-pjax-container'))
 }
 
 async function createElement(options) {
-  const refNode = document.querySelector('#repo-content-pjax-container [data-hotkey="t"]')
-  if (!refNode || document.querySelector('.gh1s-btn'))
-    return
+  const refNode = document.querySelector('#repo-content-pjax-container a.btn.d-none.d-md-block[data-hotkey="t"]')
+  if (!refNode) return
 
   const { title, icon, type, newTab } = options
+  const classList = refNode.classList.contains('ml-2') ? 'btn ml-2' : 'btn mr-2'
   const url = window.location.href.replace('https://github.com/', 'https://github1s.com/')
   const el = `
-    <a class="gh1s-btn btn mr-2" href="${url}" target="${newTab ? '_blank' : '_self'}">
+    <a id="gh1s-btn" class="${classList}" href="${url}" target="${newTab ? '_blank' : '_self'}">
       ${type === 'text' ? title : icon}
     </a>
   `
   refNode.insertAdjacentHTML('beforebegin', el)
 }
 
-function observeElement(options) {
-  createElement(options)
-  const target = document.querySelector('#repo-content-pjax-container .file-navigation')
-  if (!target) return
-  new MutationObserver((_, observer) => {
-    createElement(options)
-    observer.disconnect()
-  }).observe(target, { childList: true })
+async function init() {
+  console.log('[GitHub1s] init')
+
+  const config = await chrome.storage.sync.get()
+  const observer = new MutationObserver(
+    () => (!document.querySelector('#gh1s-btn')) && createElement(config)
+  )
+  observer.observe(
+    document.querySelector('#repo-content-pjax-container'),
+    {
+      childList: true,
+      subtree: true,
+    }
+  )
 }
 
-function isRepoPage() {
-  return document.body.contains(document.querySelector('#repo-content-pjax-container'))
-}
-
-init()
+isRepoPage() && init()
--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
-async function init() {
+document.addEventListener('DOMContentLoaded', async () => {
   const { type, newTab } = await chrome.storage.sync.get()
   form.type.value = type
   form.newTab.checked = newTab
-}
-
-document.addEventListener('DOMContentLoaded', init)
+})
 
 form.addEventListener('change', async () => {
   const type = form.type.value
